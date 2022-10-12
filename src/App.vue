fe977f235b400252513988d3e570a3f4c39569a8
Alex Law <pokemon999101@hotmail.co.uk>
2022-10-12 22:15:23 +0100

    🐛 Trim actually trims to time you pick now

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="main">
+  <div class="flexy">
     <input type="file" name="fileInput" id="fileInput" @change="fileChanged">
     <div v-if="video !== null">
       <video controls width="800" ref="video">
         <source :src="videoUrl" :type="video.type" />
       </video>
 
-      <div @click="setStart">Start</div>
-      <div @click="setEnd">End</div>
+      <div class="flexy">
+        <button @click="setStart">Start {{ start }}</button>
+        <button @click="setEnd">End {{ end }}</button>
+      </div>
     </div>
   </div>
   <div v-if="start && end">
-    <div @click="cut">Cut!</div>
+    <button @click="cut">{{ cutLabel }}</button>
   </div>
 </template>
 
@@ -25,6 +27,7 @@ export default {
       video: null,
       start: null,
       end: null,
+      cutLabel: 'Cut!'
     }
   },
   computed: {
@@ -44,6 +47,7 @@ export default {
       this.end = this.$refs.video.currentTime;
     },
     async cut(event) {
+      this.cutLabel = 'Cutting...';
       const ffmpeg = createFFmpeg({ log: true })
       await ffmpeg.load()
       const { name } = this.video;
@@ -52,21 +56,21 @@ export default {
         '-ss', `${Math.floor(this.start)}`, 
         '-i', name, 
         '-c', 'copy', 
-        '-to', `${Math.ceil(this.end)}`,
+        '-to', `${Math.ceil(this.end) - Math.floor(this.start)}`,
         'output.mp4'
       );
       const data = ffmpeg.FS('readFile', 'output.mp4');
       let blobUrl = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
       window.open(blobUrl);
       console.log('done!');
+      this.cutLabel('done!');
     }
   }
 };
 </script>
 
 <style scoped>
-
-.main {
+.flexy {
   display: flex;
   flex-direction: column;
   gap: 4px;
