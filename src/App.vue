9c5bdaa3beafe288c4b2420d23a146a74c9d3ca9
Alex Law <alexhhlaw@gmail.com>
2022-10-12 21:05:07 +0100

    ✨ Add video player

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input type="file" name="fileInput" id="fileInput" @change="fileChanged">
+    <video v-if="video !== null" controls width="800"> 
+      <source :src="videoUrl" :type="video.type" />
+    </video>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      video: null,
+    }
+  },
+  computed: {
+    videoUrl() {
+      return URL.createObjectURL(this.video)
+    }
+  },
+  methods: {
+    fileChanged(event) {
+      console.log(event.target.file)
+      this.video = event.target.files[0];
+    }
+  }
+};
+</script>
