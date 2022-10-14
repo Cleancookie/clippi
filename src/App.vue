56942c3f29ea20f8cc32321b5191302330e176b4
Alex Law <pokemon999101@hotmail.co.uk>
2022-10-14 20:56:55 +0100

    🐛 Reload video player on new file select - also reskin

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="flexy">
-    <input type="file" name="fileInput" id="fileInput" @change="fileChanged">
-    <div v-if="video !== null">
-      <video controls width="800" ref="video">
-        <source :src="videoUrl" :type="video.type" />
+  <div class="container mx-auto text-gray-100 pt-12 flex flex-col gap-12">
+    <section class="flex flex-col gap-4 items-center">
+      <h1 class="text-xl">1. Select your video 🎬</h1>
+      
+      <input type="file" name="fileInput" id="fileInput" @change="fileChanged" class="file:mr-4 file:py-2 file:px-4
+                file:rounded-full file:border-0
+                file:text-sm file:font-semibold
+                file:bg-cyan-500 file:text-white">
+    </section>
+
+    <section v-if="video !== null" class="flex flex-col gap-4 items-center">
+      <h1 class="text-xl">2. Trim your clip ⌚</h1>
+      <video controls width="800" ref="video" :key="video">
+        <source :src="videoUrl" :type="video.type"/>
       </video>
 
-      <div class="flexy">
-        <button @click="setStart">Start {{ start }}</button>
-        <button @click="setEnd">End {{ end }}</button>
+      <div class="flex gap-4">
+        <button @click="setStart" class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full">Start {{ start }}</button>
+        <button @click="setEnd" class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full">End {{ end }}</button>
       </div>
-    </div>
-  </div>
-  <div v-if="start && end">
-    <button @click="cut">{{ cutLabel }}</button>
+    </section>
+
+    <section v-if="start && end" class="flex flex-col gap-2 items-center">
+      <h1 class="text-xl">3. Trim & Save ✂️</h1>
+      <button @click="cut" class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full">{{ cutLabel }}</button>
+    </section>
   </div>
 </template>
 
@@ -27,7 +38,7 @@ export default {
       video: null,
       start: null,
       end: null,
-      cutLabel: 'Cut!'
+      cutLabel: 'Cut',
     }
   },
   computed: {
@@ -37,7 +48,7 @@ export default {
   },
   methods: {
     fileChanged(event) {
-      console.log(event.target.file)
+      console.log(event.target.files)
       this.video = event.target.files[0];
     },
     setStart(event) {
@@ -64,17 +75,9 @@ export default {
       const data = ffmpeg.FS('readFile', 'output.mp4');
       let blobUrl = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
       window.open(blobUrl);
-      console.log('done!');
-      this.cutLabel('done!');
+      this.cutLabel = 'Done';
+      setTimeout(() => { this.cutLabel = 'Cut'}, 2000)
     }
   }
 };
 </script>
-
-<style scoped>
-.flexy {
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-}
-</style>
