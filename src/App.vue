bf580061201cc7a1c97c915c11a6ecd595df0780
Alex Law <pokemon999101@hotmail.co.uk>
2023-02-16 20:35:20 +0000

    🚛 move hotkeys into mixin

--- src/App.vue
+++ src/App.vue
@@ -39,9 +39,11 @@
 
 <script>
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import hotkeys from './mixins/hotkeys.js';
 
 export default {
   name: 'app',
+  mixins: [hotkeys],
   data() {
     return {
       video: null,
@@ -63,41 +65,6 @@ export default {
 
       return `Cutting... ${Math.round(this.progress * 100)}%`;
     },
-  },
-  mounted() {
-    window.addEventListener('keydown', (e) => {
-      console.log(e.key);
-
-      // don't do anything if input type text is focused
-      if (document.activeElement.type == 'text') {
-        return;
-      }
-
-      // don't do anything if video is not loaded
-      if (!this.$refs.video) {
-        return;
-      }
-
-      if (e.key == 'ArrowRight') {
-        this.$refs.video.currentTime += 1;
-      }
-      if (e.key == 'ArrowLeft') {
-        this.$refs.video.currentTime -= 1;
-      }
-      if (e.key == ',') {
-        this.$refs.video.currentTime -= 1 / 30;
-      }
-      if (e.key == '.') {
-        this.$refs.video.currentTime += 1 / 30;
-      }
-      if (e.key == 'k' || e.key == ' ') {
-        if (this.$refs.video.paused) {
-          this.$refs.video.play();
-        } else {
-          this.$refs.video.pause();
-        }
-      }
-    });
   },
   methods: {
     fileChanged(event) {
---
+++ src/mixins/hotkeys.js
@@ -0,0 +1,37 @@
+export default {
+    mounted() {
+        window.addEventListener('keydown', (e) => {
+            console.log(e.key);
+
+            // don't do anything if input type text is focused
+            if (document.activeElement.type == 'text') {
+                return;
+            }
+
+            // don't do anything if video is not loaded
+            if (!this.$refs.video) {
+                return;
+            }
+
+            if (e.key == 'ArrowRight') {
+                this.$refs.video.currentTime += 1;
+            }
+            if (e.key == 'ArrowLeft') {
+                this.$refs.video.currentTime -= 1;
+            }
+            if (e.key == ',') {
+                this.$refs.video.currentTime -= 1 / 30;
+            }
+            if (e.key == '.') {
+                this.$refs.video.currentTime += 1 / 30;
+            }
+            if (e.key == 'k' || e.key == ' ') {
+                if (this.$refs.video.paused) {
+                    this.$refs.video.play();
+                } else {
+                    this.$refs.video.pause();
+                }
+            }
+        });
+    }
+}
